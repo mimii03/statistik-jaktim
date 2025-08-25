4bd9ecaa9aac12c9a6f403c3fa77f9aae7b0176d
Azmi Rugayyah <rugayyahazmi@gmail.com>
2025-08-25 09:06:47 +0700

    cuaks

--- tambahdata.php
+++ tambahdata.php
@@ -1,8 +1,6 @@
 <?php
-// tentukan kategori dari URL
 $type = $_GET['type'] ?? 'pendidikan';
 
-// mapping file json
 $map = [
     'pendidikan'   => 'data_pendidikan.json',
     'kesehatan'    => 'data_kesehatan.json',
@@ -12,14 +10,12 @@ $map = [
 
 $data_file = $map[$type] ?? $map['pendidikan'];
 
-// load data
 $data = [];
 if (file_exists($data_file)) {
     $json = file_get_contents($data_file);
     $data = json_decode($json, true) ?? [];
 }
 
-// hapus data
 if (isset($_GET['hapus'])) {
     $index = $_GET['hapus'];
     array_splice($data, $index, 1);
@@ -28,7 +24,6 @@ if (isset($_GET['hapus'])) {
     exit;
 }
 
-// tambah / edit data
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($type == 'pendidikan') {
         $record = [
@@ -62,7 +57,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     exit;
 }
 
-// ambil data edit
 $edit_data = null;
 $edit_index = null;
 if (isset($_GET['edit'])) {
@@ -78,8 +72,87 @@ if (isset($_GET['edit'])) {
     <title>Input data pendidikan</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body class="min-h-screen text-gray-800 dark:text-white">
+<body>
+<div class="sidebar hidden" id="sidebar">
+    <h3>Statistik</h3>
+    <a href="pendidikan.php">📚 Pendidikan</a>
+    <a href="kependudukan.php">🧑‍🤝‍🧑 Kependudukan</a>
+    <a href="ekonomi.php">💼 Ekonomi</a>
+    <a href="kesehatan.php">🏥 Kesehatan</a>
+  </div>
+
+  <div class="navbar">
+    <span class="toggle-btn" onclick="toggleSidebar()">☰</span>
+    <a href="index.php" class="beranda-link">Beranda</a>
+    
+    <div class="dropdown">
+      <input type="text" class="search-input" id="searchKel" onkeyup="filterKelurahan()" placeholder="Cari kelurahan...">
+      <div class="dropdown-content">
+        <div id="kelurahanList">
+          <a href="data.php?kelurahan=Balimester">Balimester</a>
+          <a href="data.php?kelurahan=Batu Ampar">Batu Ampar</a>
+          <a href="data.php?kelurahan=Baru">Baru</a>
+          <a href="data.php?kelurahan=Batuampar">Batuampar</a>
+          <a href="data.php?kelurahan=Bidaracina">Bidaracina</a>
+          <a href="data.php?kelurahan=Bambu Apus">Bambu Apus</a>
+          <a href="data.php?kelurahan=Cawang">Cawang</a>
+          <a href="data.php?kelurahan=Ceger">Ceger</a>
+          <a href="data.php?kelurahan=Cibubur">Cibubur</a>
+          <a href="data.php?kelurahan=Cipinang">Cipinang</a>
+          <a href="data.php?kelurahan=Cipinang Besar Selatan">Cipinang Besar Selatan</a>
+          <a href="data.php?kelurahan=Cipinang Besar Utara">Cipinang Besar Utara</a>
+          <a href="data.php?kelurahan=Cipinang Cempedak">Cipinang Cempedak</a>
+          <a href="data.php?kelurahan=Cipinang Melayu">Cipinang Melayu</a>
+          <a href="data.php?kelurahan=Cipinang Muara">Cipinang Muara</a>
+          <a href="data.php?kelurahan=Cilangkap">Cilangkap</a>
+          <a href="data.php?kelurahan=Ciracas">Ciracas</a>
+          <a href="data.php?kelurahan=Duren Sawit">Duren Sawit</a>
+          <a href="data.php?kelurahan=Dukuh">Dukuh</a>
+          <a href="data.php?kelurahan=Gedong">Gedong</a>
+          <a href="data.php?kelurahan=Halim Perdana Kusumah">Halim Perdana Kusumah</a>
+          <a href="data.php?kelurahan=Jatinegara">Jatinegara</a>
+          <a href="data.php?kelurahan=Jatinegara Kaum">Jatinegara Kaum</a>
+          <a href="data.php?kelurahan=Jati">Jati</a>
+          <a href="data.php?kelurahan=Kampung Dukuh">Kampung Dukuh</a>
+          <a href="data.php?kelurahan=Kampung Melayu">Kampung Melayu</a>
+          <a href="data.php?kelurahan=Kayu Manis">Kayu Manis</a>
+          <a href="data.php?kelurahan=Kayu Putih">Kayu Putih</a>
+          <a href="data.php?kelurahan=Kebon Manggis">Kebon Manggis</a>
+          <a href="data.php?kelurahan=Kramat Jati">Kramat Jati</a>
+          <a href="data.php?kelurahan=Klender">Klender</a>
+          <a href="data.php?kelurahan=Lubang Buaya">Lubang Buaya</a>
+          <a href="data.php?kelurahan=Malaka Jaya">Malaka Jaya</a>
+          <a href="data.php?kelurahan=Malaka Sari">Malaka Sari</a>
+          <a href="data.php?kelurahan=Makasar">Makasar</a>
+          <a href="data.php?kelurahan=Matraman">Matraman</a>
+          <a href="data.php?kelurahan=Munjul">Munjul</a>
+          <a href="data.php?kelurahan=Palmeriam">Palmeriam</a>
+          <a href="data.php?kelurahan=Pasar Rebo">Pasar Rebo</a>
+          <a href="data.php?kelurahan=Pekayon">Pekayon</a>
+          <a href="data.php?kelurahan=Penggilingan">Penggilingan</a>
+          <a href="data.php?kelurahan=Pinang Ranti">Pinang Ranti</a>
+          <a href="data.php?kelurahan=Pisangan Baru">Pisangan Baru</a>
+          <a href="data.php?kelurahan=Pondok Bambu">Pondok Bambu</a>
+          <a href="data.php?kelurahan=Pondok Kelapa">Pondok Kelapa</a>
+          <a href="data.php?kelurahan=Pondok Kopi">Pondok Kopi</a>
+          <a href="data.php?kelurahan=Pulogadung">Pulogadung</a>
+          <a href="data.php?kelurahan=Pulo Gebang">Pulo Gebang</a>
+          <a href="data.php?kelurahan=Rambutan">Rambutan</a>
+          <a href="data.php?kelurahan=Rawa Bunga">Rawa Bunga</a>
+          <a href="data.php?kelurahan=Rawa Terate">Rawa Terate</a>
+          <a href="data.php?kelurahan=Rawamangun">Rawamangun</a>
+          <a href="data.php?kelurahan=Setu">Setu</a>
+          <a href="data.php?kelurahan=Susukan">Susukan</a>
+          <a href="data.php?kelurahan=Utan Kayu Selatan">Utan Kayu Selatan</a>
+          <a href="data.php?kelurahan=Utan Kayu Utara">Utan Kayu Utara</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<div class="min-h-screen text-gray-800 dark:text-white">
     <main class="max-w-5xl mx-auto p-6">
         <form method="POST" enctype="multipart/form-data" class="space-y-3 mb-10">
             <input type="hidden" name="edit_index" value="<?= $edit_index ?>">
@@ -146,4 +219,5 @@ if (isset($_GET['edit'])) {
             </table>
         </div>
     </main>
+</div>
 </body>
