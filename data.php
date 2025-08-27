2deffe56dc5d1659100ddb6d41f0ecb1877d9211
Triandika999 <dikajoy94@gmail.com>
2025-08-27 11:52:11 +0700

    anjas

--- data.php
+++ data.php
@@ -1,29 +1,3 @@
-<?php
-session_start();
-include "koneksi.php";
-
-// cek login
-if (!isset($_SESSION['login'])) {
-    $redirectUrl = "pendidikan.php";
-    if (isset($_GET['kelurahan'])) {
-        $redirectUrl .= "?kelurahan=" . urlencode($_GET['kelurahan']);
-    }
-    header("Location: login.php?redirect=" . urlencode($redirectUrl));
-    exit;
-}
-
-$kelurahan = isset($_GET['kelurahan']) ? $_GET['kelurahan'] : '';
-$sql = "SELECT jenis_pendidikan, jumlah FROM pendidikan WHERE kelurahan='$kelurahan'";
-$result = mysqli_query($conn, $sql);
-
-$labels = [];
-$data   = [];
-
-while ($row = mysqli_fetch_assoc($result)) {
-    $labels[] = $row['jenis_pendidikan'];
-    $data[]   = $row['jumlah'];
-}
-?>
 
 <!DOCTYPE html>
 <html lang="id">
@@ -49,63 +23,28 @@ while ($row = mysqli_fetch_assoc($result)) {
     <div class="dropdown">
       <input type="text" class="search-input" id="searchKel" onkeyup="filterKelurahan()" placeholder="Cari kelurahan...">
       <div class="dropdown-content">
-        <div id="kelurahanList">
-          <a href="data.php?kelurahan=Balimester">Balimester</a>
-          <a href="data.php?kelurahan=Batu Ampar">Batu Ampar</a>
-          <a href="data.php?kelurahan=Baru">Baru</a>
-          <a href="data.php?kelurahan=Batuampar">Batuampar</a>
-          <a href="data.php?kelurahan=Bidaracina">Bidaracina</a>
-          <a href="data.php?kelurahan=Bambu Apus">Bambu Apus</a>
-          <a href="data.php?kelurahan=Cawang">Cawang</a>
-          <a href="data.php?kelurahan=Ceger">Ceger</a>
-          <a href="data.php?kelurahan=Cibubur">Cibubur</a>
-          <a href="data.php?kelurahan=Cipinang">Cipinang</a>
-          <a href="data.php?kelurahan=Cipinang Besar Selatan">Cipinang Besar Selatan</a>
-          <a href="data.php?kelurahan=Cipinang Besar Utara">Cipinang Besar Utara</a>
-          <a href="data.php?kelurahan=Cipinang Cempedak">Cipinang Cempedak</a>
-          <a href="data.php?kelurahan=Cipinang Melayu">Cipinang Melayu</a>
-          <a href="data.php?kelurahan=Cipinang Muara">Cipinang Muara</a>
-          <a href="data.php?kelurahan=Cilangkap">Cilangkap</a>
-          <a href="data.php?kelurahan=Ciracas">Ciracas</a>
-          <a href="data.php?kelurahan=Duren Sawit">Duren Sawit</a>
-          <a href="data.php?kelurahan=Dukuh">Dukuh</a>
-          <a href="data.php?kelurahan=Gedong">Gedong</a>
-          <a href="data.php?kelurahan=Halim Perdana Kusumah">Halim Perdana Kusumah</a>
-          <a href="data.php?kelurahan=Jatinegara">Jatinegara</a>
-          <a href="data.php?kelurahan=Jatinegara Kaum">Jatinegara Kaum</a>
-          <a href="data.php?kelurahan=Jati">Jati</a>
-          <a href="data.php?kelurahan=Kampung Dukuh">Kampung Dukuh</a>
-          <a href="data.php?kelurahan=Kampung Melayu">Kampung Melayu</a>
-          <a href="data.php?kelurahan=Kayu Manis">Kayu Manis</a>
-          <a href="data.php?kelurahan=Kayu Putih">Kayu Putih</a>
-          <a href="data.php?kelurahan=Kebon Manggis">Kebon Manggis</a>
-          <a href="data.php?kelurahan=Kramat Jati">Kramat Jati</a>
-          <a href="data.php?kelurahan=Klender">Klender</a>
-          <a href="data.php?kelurahan=Lubang Buaya">Lubang Buaya</a>
-          <a href="data.php?kelurahan=Malaka Jaya">Malaka Jaya</a>
-          <a href="data.php?kelurahan=Malaka Sari">Malaka Sari</a>
-          <a href="data.php?kelurahan=Makasar">Makasar</a>
-          <a href="data.php?kelurahan=Matraman">Matraman</a>
-          <a href="data.php?kelurahan=Munjul">Munjul</a>
-          <a href="data.php?kelurahan=Palmeriam">Palmeriam</a>
-          <a href="data.php?kelurahan=Pasar Rebo">Pasar Rebo</a>
-          <a href="data.php?kelurahan=Pekayon">Pekayon</a>
-          <a href="data.php?kelurahan=Penggilingan">Penggilingan</a>
-          <a href="data.php?kelurahan=Pinang Ranti">Pinang Ranti</a>
-          <a href="data.php?kelurahan=Pisangan Baru">Pisangan Baru</a>
-          <a href="data.php?kelurahan=Pondok Bambu">Pondok Bambu</a>
-          <a href="data.php?kelurahan=Pondok Kelapa">Pondok Kelapa</a>
-          <a href="data.php?kelurahan=Pondok Kopi">Pondok Kopi</a>
-          <a href="data.php?kelurahan=Pulogadung">Pulogadung</a>
-          <a href="data.php?kelurahan=Pulo Gebang">Pulo Gebang</a>
-          <a href="data.php?kelurahan=Rambutan">Rambutan</a>
-          <a href="data.php?kelurahan=Rawa Bunga">Rawa Bunga</a>
-          <a href="data.php?kelurahan=Rawa Terate">Rawa Terate</a>
-          <a href="data.php?kelurahan=Rawamangun">Rawamangun</a>
-          <a href="data.php?kelurahan=Setu">Setu</a>
-          <a href="data.php?kelurahan=Susukan">Susukan</a>
-          <a href="data.php?kelurahan=Utan Kayu Selatan">Utan Kayu Selatan</a>
-          <a href="data.php?kelurahan=Utan Kayu Utara">Utan Kayu Utara</a>
+          <?php
+        $listKelurahan = [
+          "Balimester", "Batu Ampar", "Baru", "Batuampar", "Bidaracina",
+          "Bambu Apus", "Cawang", "Ceger", "Cibubur", "Cipinang",
+          "Cipinang Besar Selatan", "Cipinang Besar Utara", "Cipinang Cempedak",
+          "Cipinang Melayu", "Cipinang Muara", "Cilangkap", "Ciracas",
+          "Duren Sawit", "Dukuh", "Gedong", "Halim Perdana Kusumah",
+          "Jatinegara", "Jatinegara Kaum", "Jati", "Kampung Dukuh",
+          "Kampung Melayu", "Kayu Manis", "Kayu Putih", "Kebon Manggis",
+          "Kramat Jati", "Klender", "Lubang Buaya", "Malaka Jaya",
+          "Malaka Sari", "Makasar", "Matraman", "Munjul", "Palmeriam",
+          "Pasar Rebo", "Pekayon", "Penggilingan", "Pinang Ranti",
+          "Pisangan Baru", "Pondok Bambu", "Pondok Kelapa", "Pondok Kopi",
+          "Pulogadung", "Pulo Gebang", "Rambutan", "Rawa Bunga",
+          "Rawa Terate", "Rawamangun", "Setu", "Susukan",
+          "Utan Kayu Selatan", "Utan Kayu Utara"
+        ];
+
+        foreach ($listKelurahan as $nama) {
+            echo "<a href='pendidikan.php?kelurahan=" . urlencode($nama) . "'>$nama</a>";
+        }
+        ?>
         </div>
       </div>
     </div>
