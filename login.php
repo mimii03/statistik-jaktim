a6ac6d14008c0a58da86d25193e7e48a6336e737
Triandika999 <dikajoy94@gmail.com>
2025-08-27 09:14:27 +0700

    nfwifnwisfw

--- login.php
+++ login.php
@@ -9,6 +9,9 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 
 $error = '';
 
+// simpan query redirect kalau ada
+$redirect = isset($_GET['redirect']) ? $_GET['redirect'] : 'index.php';
+
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -19,7 +22,10 @@ if (isset($_POST['login'])) {
     if ($row && password_verify($password, $row['password'])) {
         $_SESSION['login'] = true;
         $_SESSION['username'] = $row['username'];
-        header("Location: index.php");
+
+        // ambil dari POST hidden input
+        $redirect = isset($_POST['redirect']) ? $_POST['redirect'] : 'index.php';
+        header("Location: $redirect");
         exit;
     } else {
         $error = "Username atau Password salah!";
@@ -38,6 +44,7 @@ if (isset($_POST['login'])) {
     <h2>Login</h2>
     <?php if ($error) echo "<div class='message'>$error</div>"; ?>
     <form method="post">
+      <input type="hidden" name="redirect" value="<?php echo htmlspecialchars($redirect); ?>">
       <input type="text" name="username" placeholder="Username" required>
       <input type="password" name="password" placeholder="Password" required>
       <button type="submit" name="login">Login</button>
